da4b71155e983dc6e1818fcb3499ef838da5aec2
T-SravaniReddy <69198531+T-SravaniReddy@users.noreply.github.com>
2021-04-28 15:09:57 +0530

    Add files via upload

---
+++ 18WH1A05A0_Program1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+probAbandFri=0.03
+probFri=0.2
+result = (probAbandFri / probFri)
+print(result * 100)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
